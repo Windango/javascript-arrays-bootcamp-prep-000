49af0513f60694358443afe8555fdb45b6d02bb6
robertlylegay@gmail.com <robertlylegay@gmail.com>
2018-04-10 17:48:40 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray("foo",1){
+  const array = ["Milo", "Garfield"]
+ 
+// we need a new variable:
+const array = ["Felix", ...array]
+}
+addElementToBeginningOfArray("numbers",4)
+
+function destructivelyAddElementToBeginningOfArray(a,e){
+  
+}
+
+function addElementToEndOfArray(){
+  
+}
+
+function destructivelyAddElementToEndOfArray(){
+  
+}
